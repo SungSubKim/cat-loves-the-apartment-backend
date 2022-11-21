37c7433c54439ab133dad50f2e222293862e214b
박일규 <repeater1384@naver.com>
2022-11-21 17:36:49 +0900

    [Add] 회원정보수정(일반유저), 비밀번호초기화, 메일전송 기능구현

--- src/main/java/com/ssafy/vue/controller/MemberController.java
+++ src/main/java/com/ssafy/vue/controller/MemberController.java
@@ -10,18 +10,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.vue.model.MemberDto;
 import com.ssafy.vue.model.service.JwtServiceImpl;
 import com.ssafy.vue.model.service.MemberService;
+import com.ssafy.vue.model.service.SendMailService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -35,16 +34,17 @@ public class MemberController {
 	public static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
-	
+
 	@Autowired
 	private JwtServiceImpl jwtService;
 
 	@Autowired
 	private MemberService memberService;
-	
+	@Autowired
+	private SendMailService sendMailService;
+
 	@PostMapping("/join")
-	public ResponseEntity<String> join(
-			@RequestBody MemberDto memberDto) {
+	public ResponseEntity<String> join(@RequestBody MemberDto memberDto) {
 		logger.debug("memberDto info : {}", memberDto);
 		try {
 			memberService.joinMember(memberDto);
@@ -54,12 +54,24 @@ public class MemberController {
 			return new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
+	@PostMapping("/init-pwd")
+	public ResponseEntity<String> initPwd(@RequestBody Map<String, Object> temp) {
+
+		logger.debug("param:{}", temp);
+		String email = (String) temp.get("email");
+		String initializedPwd = (String) temp.get("initializePwd");
+		
+		sendMailService.sendInitializedPwd(email, initializedPwd);
+		
+		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+	}
+
 	@ApiOperation(value = "로그인", notes = "Access-token과 로그인 결과 메세지를 반환한다.", response = Map.class)
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, Object>> login(
 			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) MemberDto memberDto) {
-		logger.info("login controller, {}",memberDto.toString());
+		logger.info("login controller, {}", memberDto.toString());
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status = null;
 		try {
@@ -81,7 +93,7 @@ public class MemberController {
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	
+
 	@ApiOperation(value = "회원인증", notes = "회원 정보를 담은 Token을 반환한다.", response = Map.class)
 	@GetMapping("/{userid}")
 	public ResponseEntity<Map<String, Object>> getInfo(
@@ -92,17 +104,17 @@ public class MemberController {
 		HttpStatus status = HttpStatus.ACCEPTED;
 //		if (jwtService.isUsable(request.getHeader("access-token"))) {
 //			logger.info("사용 가능한 토큰!!!");
-			try {
+		try {
 //				로그인 사용자 정보.
-				MemberDto memberDto = memberService.userInfo(userid);
-				resultMap.put("userInfo", memberDto);
-				resultMap.put("message", SUCCESS);
-				status = HttpStatus.ACCEPTED;
-			} catch (Exception e) {
-				logger.error("정보조회 실패 : {}", e);
-				resultMap.put("message", e.getMessage());
-				status = HttpStatus.INTERNAL_SERVER_ERROR;
-			}
+			MemberDto memberDto = memberService.userInfo(userid);
+			resultMap.put("userInfo", memberDto);
+			resultMap.put("message", SUCCESS);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			logger.error("정보조회 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
 //		} else {
 //			logger.error("사용 불가능 토큰!!!");
 //			resultMap.put("message", FAIL);
